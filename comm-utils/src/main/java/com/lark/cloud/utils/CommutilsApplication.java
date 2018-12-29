15b1d21ac52525e66c3a808164d49be00b56d9a3
Lark <117561271@qq.com>
2018-12-29 21:43:23 +0800

    入口类提交

---
+++ comm-utils/src/main/java/com/lark/cloud/utils/CommutilsApplication.java
@@ -0,0 +1,14 @@
+package com.lark.cloud.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommutilsApplication.class, args);
+	}
+
+}
+
